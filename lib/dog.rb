bddca2908198d8eb8cd86ef282cc64f5117af86a
Marianne Pyon <mariannepyon@gmail.com>
2019-08-13 17:59:13 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+    def initialize(name, breed)
+      @name = name
+      @breed = breed
+    end
+    def breed=(breed)
+      @breed = breed
+    if breed = 0
+      puts "Mutt"
+    end
+  end
+
+    def breed
+      @breed
+    end
+  end
